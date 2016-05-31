23e18d1d9aec5532bea2c5c35769c5e0d721e1da
Andreas Umbach <umbach@gmail.com>
2016-05-31 10:18:39 +0200

    animation of multiple stars spiraling into the center

--- js/Rotationen3x3.js
+++ js/Rotationen3x3.js
@@ -1,8 +1,8 @@
-var phi = 0;
+var t = 0;
 
 function setup() 
 {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
     
   background(255);
   stroke(0);
@@ -21,13 +21,8 @@ function drawShape(points)
   endShape(CLOSE);
 }
 
-function draw()
-{	
-  var dimension = min(width, height);
-  translate(width/2, height/2);
-  scale(dimension / 2, dimension / 2);
-  strokeWeight(0.02);
-  
+function drawStar(m)
+{
   var k = 0.2;
   var p = [
      new Point(1, 0),
@@ -40,20 +35,48 @@ function draw()
      new Point(k, -k),
      // new Point(1, 0)
   ];
-  
-  phi += PI / 36;
-  var s = 0.5 * sin(1.5 * phi);
-  fill(144 + 72 * s);
-    
-  var m1 = Matrix3x3.rotate(phi);
-  var m2 = Matrix3x3.scale(s);
-  var m3 = Matrix3x3.translate(0.5 * cos(phi), 0.5 * sin(phi))
-  var m = Matrix3x3.multiply(m3, Matrix3x3.multiply(m2, m1));
-  
   for(var i in p)
   {
     p[i].transform(m);
   }
+  drawShape(p);	
+}
+
+function draw()
+{
+   // clear();
+	
+  t += PI / 36;
+  while(t > 8 * PI) { t -= 8 * PI; clear(); }
   
-  drawShape(p);
+  	
+  var dimension = min(width, height);
+  translate(width/2, height/2);
+  scale(dimension / 2, -dimension / 2);
+  strokeWeight(0.02);
+  
+  
+
+  
+  // n Sterne
+  var n = 6;
+  
+  for(var i = 0; i < n; i++)
+  {
+	  var phi0 = 2 * PI / n * i;
+	  var phi = phi0 + t;
+	  var r = pow(2, -1 / PI * t);
+	  var s = 1.0 / 32.0 * r;
+	  
+	  // print("phi: " + phi + ", r: " + r + ", s: " + s);
+	  
+	  var m1 = Matrix3x3.scale(s);
+	  var m2 = Matrix3x3.translate(r, 0);
+	  var m3 = Matrix3x3.rotate(phi);
+	  var m = Matrix3x3.multiply(m3, Matrix3x3.multiply(m2, m1));
+	  
+	  strokeWeight(0.1 * s);
+
+	  drawStar(m);
+  }
 }
